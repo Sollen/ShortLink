1dccd71933cf2f649a2d4e7b9751eb71eb9c4c94
Sollen <sollen53@outlook.com>
2018-06-27 23:38:05 +0500

    Begin Entity Test

---
+++ ShortLink.Test/EntityTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ShortLink.Helpers;
+using ShortLink.Models;
+using Xunit;
+
+namespace ShortLink.Test
+{
+    public class EntityTest
+    {
+        [Fact]
+        public void testGetUser()
+        {
+            UserRepository users = new UserRepository();
+            User user = users.GetUser("User1");
+            Assert.Equal(user.Login, new User(){Login = "User1"}.Login); 
+
+        }
+    }
+}
---
+++ ShortLink/Helpers/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShortLink.Models;
+
+namespace ShortLink.Helpers
+{
+    public class UserRepository
+    {
+        public User GetUser(string login)
+        {
+            return new User{Login = login};
+        }
+    }
+}
